3ef233384abf6bd56b1876a2edbf400b316a6b09
daniellalsopp <112625780+daniellalsopp@users.noreply.github.com>
2022-10-04 17:28:35 +0100

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,18 @@
+name = input("Welcome! Please enter your name: ")
+
+print("Hello, " + (name) + "!")
+
+end = input("Let's play Fizzbuzz. Please enter a number between 1 and 100: ")
+end = int(end)  # convert string into number
+
+for num in range(1, end+1):
+    if num % 3 == 0 and num % 5 == 0:
+        print("fizzbuzz")
+    elif num % 3 == 0:
+        print("fizz")
+    elif num % 5 == 0:
+        print("buzz")
+    else:
+        print(num)
+
+print("Thank you for playing!")
